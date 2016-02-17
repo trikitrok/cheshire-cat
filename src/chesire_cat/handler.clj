cf9a57c5346ea9f85c242cb010e0bcb6fc4483c1
trikitrok <trikitrok@gmail.com>
2016-02-17 19:18:06 +0100

    JSON endpoint

---
+++ src/chesire_cat/handler.clj
@@ -0,0 +1,19 @@
+(ns chesire-cat.handler
+  (:require
+    [compojure.core :refer :all]
+    [compojure.route :as route]
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+    [cheshire.core :as json]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (GET "/cheshire-cat" []
+    {:status 200
+     :headers {"Content-Type" "application/json; charset=utf-8"}
+     :body (json/generate-string
+             {:name "Cheshire Cat"
+              :status :grinning})})
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
