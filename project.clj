64094068607c28785b93b0a763ab53c36f72ddbb
trikitrok <trikitrok@gmail.com>
2016-02-19 21:38:00 +0100

    Using cljs-http and core.async to get data from the endpoint

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
                  [ring/ring-json "0.3.1"]
-                 [org.clojure/clojurescript "0.0-2371"]]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [cljs-http "0.1.18"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :plugins [[lein-ring "0.8.13"]
             [lein-cljsbuild "1.0.3"]]
   :ring {:handler chesire-cat.handler/app}
--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -1,4 +1,16 @@
-(ns cheshire-cat.core)
+(ns cheshire-cat.core
+  (:require-macros
+    [cljs.core.async.macros :refer [go]])
+  (:require
+    [clojure.browser.repl :as repl]
+    [cljs-http.client :as http]
+    [cljs.core.async :refer [<!]]))
+
+(defn log [sth]
+  (.log js/console (pr-str sth)))
 
 (defn ^:export init []
-  (js/alert "hi"))
+  (repl/connect "http://localhost:9000/repl")
+  (go
+    (let [response (<! (http/get "/cheshire-cat"))]
+      (log (:body response)))))
